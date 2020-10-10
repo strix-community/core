d20018059c90f992ab01f232adac0ac771000a03
Robert Dennis <robertdrakedennis@users.noreply.github.com>
2020-10-10 21:15:33 +0000

    Apply fixes from StyleCI

--- routes/strix-api.php
+++ routes/strix-api.php
@@ -9,9 +9,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
